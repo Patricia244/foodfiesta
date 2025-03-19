9d1175a9e40929fc5493e40f7dcce0720b94dc73
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2025-03-20 00:32:00 +0200

    remove duplicated cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,35 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRoute from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRoute from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRoute from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
-const app = express()
-app.use(cors({
-    origin: process.env.FRONTEND_URL|| process.env.HOSTED_FRONTEND || process.env.HOSTED_ADMIN, 
-    credentials: true, 
-  }))
-app.use(express.json())
-const port = 4000
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || process.env.HOSTED_FRONTEND || process.env.HOSTED_ADMIN,
+    credentials: true,
+  })
+);
 
-connectDB()
+app.use(express.json());
 
-//api endpoint
-app.use("/api/food", foodRoute)
-app.use("/uploads", express.static("uploads"))
-app.use("/api/user",userRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRoute)
+const port = 4000;
+
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRoute);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRoute);
 
 app.listen(port, () => {
-    console.log(`Server is running on port  http://localhost:${port}`)
-    })
-
-
+  console.log(`Server is running on port http://localhost:${port}`);
+});
